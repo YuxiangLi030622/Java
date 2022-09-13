61351c7448e4e6fbb9dd751f36d56166904667a6
YuxiangLi030622 <lyx20030622@163.com>
2022-09-13 19:13:07 +0800

    first commit

---
+++ Java001/src/HelloJava.java
@@ -0,0 +1,5 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("This is my first Java program");//输出
+    }
+}
---
+++ Java001/src/IndiviualInformation.java
@@ -0,0 +1,8 @@
+public class IndiviualInformation {
+    public static void main(String[] args) {
+        System.out.println("姓名：李欲翔");
+        System.out.println("性别：男");
+        System.out.println("籍贯：四川乐山");
+        System.out.println("住址：武汉大学信息学部国软c3-509");
+    }
+}
